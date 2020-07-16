031895e976825d96604e49b866d45a3db8bd63a7
lidium <elesahich@gmail.com>
2020-07-16 12:20:29 +0900

    [Feat] 플레이리스트 뷰 서버 왜 터지죠??????

--- placepic/placepic/HomeScene/Model/Enums/categoryIndex.swift
+++ placepic/placepic/HomeScene/Model/Enums/categoryIndex.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+enum CategoryIndex: String {
+    case one = "전체"
+    case two = "맛집"
+    case three = "술집"
+    case four = "카페"
+    case five = "스터디"
+}
+
+extension CategoryIndex {
+    var index: Int {
+        switch self {
+        case .one:
+            return 1
+        case .two:
+            return 2
+        case .three:
+            return 3
+        case .four:
+            return 4
+        case .five:
+            return 5
+        }
+    }
+}
+
